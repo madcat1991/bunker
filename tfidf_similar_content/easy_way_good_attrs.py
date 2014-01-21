e67d6411c24f14cec8cbc08329da3bd8470f9193
Tural Gurbanov <madcat1991@gmail.com>
2014-01-21 22:13:21 +0400

    Версия с возможность указывать только повторяющиеся тэги

--- tfidf_similar_content/easy_way_good_attrs.py
+++ tfidf_similar_content/easy_way_good_attrs.py
@@ -1,7 +1,18 @@
 #coding: utf-8
 
+"""
+Версия в которой берутся только атрибуты, которые встречаются не менее чем у n
+контентов
+"""
+import argparse
+import logging
+import psycopg2
+import sys
 
-class NewContent(object):
+logger = logging.getLogger("easy_way_good_attrs")
+
+
+class Content(object):
     def __init__(self, content_id, title):
         self.id = content_id
         self.title = title
@@ -56,59 +67,63 @@ property_sql = """
 content_sql = "SELECT id, title FROM content"
 
 
-def collect_data_new():
+def collect_data(args):
     data = {}
-    conn = psycopg2.connect(database="da_prod", user="da", host="localhost", port="5432")
+    conn = psycopg2.connect(
+        database=args.db_name,
+        user=args.db_user,
+        host=args.db_host,
+        port=args.db_port
+    )
     cursor = conn.cursor()
 
-    print "COLLECTING DATA"
     cursor.execute(content_sql)
     for c_id, c_title in cursor:
-        content = NewContent(c_id, c_title)
+        content = Content(c_id, c_title)
         data[c_id] = content
-    print "CONTENT DATA COLLECTED"
+    logger.info("CONTENT DATA COLLECTED")
 
     cursor.execute(category_sql)
     for attr_id, content_ids in cursor:
-        if content_ids and len(content_ids) >= 2:
+        if content_ids and len(content_ids) >= args.limit_n:
             for c_id in content_ids:
                 data[c_id].categories.add(attr_id)
-    print "CATEGORIES COLLECTED"
+    logger.info("CATEGORIES COLLECTED")
 
     cursor.execute(genre_sql)
     for attr_id, content_ids in cursor:
-        if content_ids and len(content_ids) >= 2:
+        if content_ids and len(content_ids) >= args.limit_n:
             for c_id in content_ids:
                 data[c_id].genres.add(attr_id)
-    print "GENRES COLLECTED"
+    logger.info("GENRES COLLECTED")
 
     cursor.execute(award_sql)
     for attr_id, content_ids in cursor:
-        if content_ids and len(content_ids) >= 2:
+        if content_ids and len(content_ids) >= args.limit_n:
             for c_id in content_ids:
                 data[c_id].awards.add(attr_id)
-    print "AWARDS COLLECTED"
+    logger.info("AWARDS COLLECTED")
 
     cursor.execute(tag_sql)
     for attr_id, content_ids in cursor:
-        if content_ids and len(content_ids) >= 2:
+        if content_ids and len(content_ids) >= args.limit_n:
             for c_id in content_ids:
                 data[c_id].tags.add(attr_id)
-    print "TAGS COLLECTED"
+    logger.info("TAGS COLLECTED")
 
     cursor.execute(person_sql)
     for attr_id, content_ids in cursor:
-        if content_ids and len(content_ids) >= 2:
+        if content_ids and len(content_ids) >= args.limit_n:
             for c_id in content_ids:
                 data[c_id].persons.add(attr_id)
-    print "PERSONS COLLECTED"
+    logger.info("PERSONS COLLECTED")
 
     cursor.execute(property_sql)
     for attr_id, content_ids in cursor:
-        if content_ids and len(content_ids) >= 2:
+        if content_ids and len(content_ids) >= args.limit_n:
             for c_id in content_ids:
                 data[c_id].properties.add(c_id)
-    print "PROPERTIES COLLECTED"
+    logger.info("PROPERTIES COLLECTED")
     conn.close()
 
     def check(dict_entity):
@@ -117,7 +132,7 @@ def collect_data_new():
     return dict(filter(check, data.iteritems()))
 
 
-def get_similarity_new(content1, content2):
+def get_similarity(content1, content2):
     up = len(content1.categories.intersection(content2.categories))
     up += len(content1.genres.intersection(content2.genres))
     up += len(content1.tags.intersection(content2.tags))
@@ -135,12 +150,39 @@ def get_similarity_new(content1, content2):
     return 0
 
 
-def find_similar_content_new(data, similar_to=64252, top=20):
+def find_similar_content(data, similar_to=64252, top=20):
     result = {}
     main_content = data.pop(similar_to)
     for content_id, other_content in data.iteritems():
-        result[other_content] = get_similarity_new(main_content, other_content)
+        result[other_content] = get_similarity(main_content, other_content)
 
     result = sorted(result.iteritems(), key=lambda x: x[1], reverse=True)
     for k, v in result[:top]:
         print k, "-", v
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=u'Скрипт для поиска похожего контента')
+    parser.add_argument('--db_host', default="localhost", type=str, dest="db_host",
+                        help=u'Хост БД, по умолчанию: localhost')
+    parser.add_argument('--db_port', default="5432", type=str, dest="db_port",
+                        help=u'Порт БД, по умолчанию: 5432')
+    parser.add_argument('--db_name', default="da_prod", type=str, dest="db_name",
+                        help=u'Имя БД, по умолчанию: da')
+    parser.add_argument('--db_user', default="da", type=str, dest="db_user",
+                        help=u'Пользователь БД, по умолчанию: da')
+    parser.add_argument('-c', default=64252, type=int, dest="similar_to",
+                        help=u'Идентификатор контента для которого будут искаться похожие. '
+                             u'По умолчанию: 64252 (Темный рыцарь)')
+    parser.add_argument('-n', default=2, type=int, dest="limit_n",
+                        help=u'Количество единиц контента, которые должны встречаться с '
+                             u'атрибутом. По умолчанию: 2')
+    parser.add_argument('-t', default=20, type=int, dest="top",
+                        help=u'Размер топа результатов. '
+                             u'По умолчанию находятся первые 20 похожих единиц контента')
+
+    args = parser.parse_args()
+    logging.basicConfig(stream=sys.stdout, level="INFO", format="%(asctime)s :: %(message)s")
+    data = collect_data(args)
+    logger.info("DATA COLLECTED")
+    find_similar_content(data, similar_to=args.similar_to, top=args.top)
